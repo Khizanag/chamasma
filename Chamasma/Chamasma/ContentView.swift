f33435cf5310916cd54cacb2782c0ece389cc689
Giga Khizanishvili <khizanag@gmail.com>
2023-04-15 23:33:31 +0400

    Integrate CodeScanner

--- Chamasma/Chamasma/ContentView.swift
+++ Chamasma/Chamasma/ContentView.swift
@@ -5,19 +5,67 @@
 //  Created by Giga Khizanishvili on 14.04.23.
 //
 
+import CodeScanner
 import CoreData
+import DesignSystem
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+    @State private var isScannerPresented = false
 
     var body: some View {
-        Color.red
+        NavigationStack {
+            Color.green
+                .ignoresSafeArea(edges: .bottom )
+                .toolbar {
+                    Button {
+                        isScannerPresented = true
+                    } label: {
+                        Label("Scan", systemImage: "qrcode.viewfinder")
+                    }
+                }
+                .sheet(isPresented: $isScannerPresented) {
+                    CodeScannerView(
+                        codeTypes: [.qr],
+                        simulatedData: "Giga Kh",
+                        completion: handlerScanResult
+                    )
+                }
+        }
+    }
+
+    // MARK: - Methods
+    private func handlerScanResult(_ result: Result<ScanResult, ScanError>) {
+        switch result {
+        case .success(let scanResult):
+            let content = scanResult.string
+            print("Content of the code is \(content)")
+        case .failure(let error):
+            switch error {
+            case .badInput:
+                print("Bad input for QR")
+            case .badOutput:
+                print("Bad output for QR")
+            case .initError(let error):
+                print("\(error.localizedDescription)")
+            case .permissionDenied:
+                print("Permission Denied")
+            }
+        }
+
+        isScannerPresented = false
+    }
+}
+
+// MARK: - Constant
+private extension ContentView {
+    enum Constant {
+        static let size: CGFloat = 200
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
