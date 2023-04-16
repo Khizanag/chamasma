e86c1f6389bd172be347346654346b370caf1e5f
Giga Khizanishvili <khizanag@gmail.com>
2023-04-16 17:20:19 +0400

    Create component for MenuCategory Item

--- Chamasma/DesignSystem/Sources/DesignSystem/Components/DesignSystem+Image.swift
+++ Chamasma/DesignSystem/Sources/DesignSystem/Components/DesignSystem+Image.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 public extension DesignSystem {
     enum Image {
+        public static let burger = SwiftUI.Image("burger")
         public static let leafFilled = SwiftUI.Image(systemName: "leaf.fill")
     }
 }
