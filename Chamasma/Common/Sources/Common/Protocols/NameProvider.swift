5f884c4fdd78f594e09e3bd5e2f89fe2bc69032d
Giga Khizanishvili <khizanag@gmail.com>
2023-04-17 20:27:18 +0400

    Create `NameProvider` protocol

---
+++ Chamasma/Common/Sources/Common/Protocols/NameProvider.swift
@@ -0,0 +1,18 @@
+//
+//  NameProvider.swift
+//  
+//
+//  Created by Giga Khizanishvili on 17.04.23.
+//
+
+// MARK: - Protocol
+public protocol NameProvider {
+    var name: String { get }
+}
+
+// MARK: - Extension
+public extension NameProvider {
+    var name: String {
+        String(reflecting: self)
+    }
+}
