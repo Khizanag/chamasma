bd6bc097641ebe1dfc502fc3228aa29713b0c750
Giga Khizanishvili <giga.khizanishvili@space.ge>
2023-04-21 01:01:50 +0400

    Create URLRequestBuilder

--- Chamasma/Presentation/Package.swift
+++ Chamasma/Presentation/Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         .iOS(.v16),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Presentation",
             targets: ["Presentation"]),
@@ -18,8 +17,6 @@ let package = Package(
         .package(path: "../Domain"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Presentation",
             dependencies: [
