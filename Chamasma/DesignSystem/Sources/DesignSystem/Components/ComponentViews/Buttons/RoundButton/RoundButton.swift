6ab5900d3c8a0f5b0d28fa76b52014bfe9fe20bb
Mariam Ormotsadze <Mariam.Ormotsadze@space.ge>
2023-05-05 16:43:46 +0400

    add round button

---
+++ Chamasma/DesignSystem/Sources/DesignSystem/Components/ComponentViews/Buttons/RoundButton/RoundButton.swift
@@ -0,0 +1,42 @@
+//
+//  RoundButton.swift
+//
+//
+//  Created by Mariam Ormotsadze on 05.05.23.
+//
+
+import Common
+import SwiftUI
+
+struct RoundButton: View {
+    let model: RoundButtonModel
+
+    init(model: RoundButtonModel) {
+        self.model = model
+    }
+
+    var body: some View {
+        Button {
+            print("ra")
+        } label: {
+            model.image.name
+                .frame(width: model.size.value, height: model.size.value)
+                .font(.system(size: model.size.font))
+                .foregroundColor(model.state.foregroundColor)
+                .background(model.state.backgroundColor)
+                .clipShape(Circle())
+        }
+    }
+}
+
+struct RoundButton_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundButton(
+            model: .init(
+                image: .backArrow,
+                state: .filled,
+                size: .large
+            )
+        )
+    }
+}
