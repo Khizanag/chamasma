4ca033a1b445a7334355af0e189cada660d7186a
Mariam Ormotsadze <Mariam.Ormotsadze@space.ge>
2023-05-10 13:06:49 +0400

    refactor RoundButton

--- Chamasma/DesignSystem/Sources/DesignSystem/Components/ComponentViews/Buttons/RoundButton/RoundButton.swift
+++ Chamasma/DesignSystem/Sources/DesignSystem/Components/ComponentViews/Buttons/RoundButton/RoundButton.swift
@@ -21,10 +21,65 @@ struct RoundButton: View {
         } label: {
             model.image.name
                 .frame(width: model.size.value, height: model.size.value)
-                .font(.system(size: model.size.font))
+                .font(.system(size: model.size.fontSize))
                 .foregroundColor(model.state.foregroundColor)
                 .background(model.state.backgroundColor)
                 .clipShape(Circle())
+        }
+    }
+}
+
+private extension RoundButtonImage {
+    var name: Image {
+        switch self {
+        case .plus:
+            return Image(systemName: "plus")
+        case .search:
+            return Image(systemName: "magnifyingglass")
+        case .backArrow:
+            return Image(systemName: "arrow.backward")
+        case .close:
+            return Image(systemName: "xmark")
+        }
+    }
+}
+
+private extension RoundButtonState {
+    var backgroundColor: Color {
+        switch self {
+        case .filled:
+            return Color(hex: "4ED199") ?? .black
+        case .clear:
+            return .clear
+        }
+    }
+
+    var foregroundColor: Color {
+        switch self {
+        case .filled:
+            return .white
+        case .clear:
+            return Color(hex: "4ED199") ?? .black
+        }
+    }
+}
+
+private extension RoundButtonSize {
+    var value: CGFloat {
+        switch self {
+        case .large:
+            return 100
+        case .normal:
+            return 40
+        }
+    }
+
+    var fontSize: CGFloat {
+        switch self {
+        case .large:
+            return 50
+        case .normal:
+            return 20
         }
     }
 }
@@ -33,9 +88,9 @@ struct RoundButton_Previews: PreviewProvider {
     static var previews: some View {
         RoundButton(
             model: .init(
-                image: .backArrow,
+                image: .plus,
                 state: .filled,
-                size: .large
+                size: .normal
             )
         )
     }
