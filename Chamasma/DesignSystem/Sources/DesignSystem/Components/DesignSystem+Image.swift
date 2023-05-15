da01235347258a955dc1c8b6a99d387b2a7a0147
Mariam Ormotsadze <Mariam.Ormotsadze@space.ge>
2023-05-16 01:40:58 +0400

    complete codereview recommendations

--- Chamasma/DesignSystem/Sources/DesignSystem/Components/DesignSystem+Image.swift
+++ Chamasma/DesignSystem/Sources/DesignSystem/Components/DesignSystem+Image.swift
@@ -11,6 +11,10 @@ public extension DesignSystem {
     enum Image {
         public static let burger = SwiftUI.Image("burger")
         public static let leafFilled = SwiftUI.Image(systemName: "leaf.fill")
+        public static let plus = SwiftUI.Image(systemName: "plus")
+        public static let magnifyingGlass = SwiftUI.Image(systemName: "magnifyingglass")
+        public static let arrowBackward = SwiftUI.Image(systemName: "arrow.backward")
+        public static let xMark = SwiftUI.Image(systemName: "xmark")
 
         public enum Star {
             public static let unfilled = SwiftUI.Image(systemName: "star")
