6e9e2f9c788bb0f77db215f7c90c4d3b7f8708c9
Giga Khizanishvili <giga.khizanishvili@space.ge>
2023-06-02 12:10:11 +0400

    Add new colors into DesignSystem

--- Chamasma/DesignSystem/Sources/DesignSystem/Components/DesignSystem+Color.swift
+++ Chamasma/DesignSystem/Sources/DesignSystem/Components/DesignSystem+Color.swift
@@ -5,6 +5,32 @@
 //  Created by Giga Khizanishvili on 14.04.23.
 //
 
+import Common
+import SwiftUI
+
 public extension DesignSystem {
-    enum Color { }
+    enum Color {
+        case hexE17E76
+        case hexE3BAA8
+        case hexAA7B6F
+        case hexFED797
+        case hex2E8364
+
+        case hex4ED199
+        case hex41E3A2
+        case hex4aBF90
+        case hexBBD8CB
+        case hexFFBC10
+        case hex6D9FEB
+        case hexFAხხ030
+        case hex646464
+        case hex202020
+        case hexF78F8E
+
+        func callAsFunction() -> SwiftUI.Color {
+            SwiftUI.Color(name.dropFirst(3).toString())
+        }
+    }
 }
+
+extension DesignSystem.Color: NameProvider { }
