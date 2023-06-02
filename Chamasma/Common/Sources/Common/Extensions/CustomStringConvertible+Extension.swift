2e872cafa48eecc496b57a1ce401c58c074f1a92
Giga Khizanishvili <giga.khizanishvili@space.ge>
2023-06-02 17:18:39 +0400

    Create CustomStringConvertible's toString extension

---
+++ Chamasma/Common/Sources/Common/Extensions/CustomStringConvertible+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  CustomStringConvertible+Extension.swift
+//
+//
+//  Created by Giga Khizanishvili on 02.06.23.
+//
+
+public extension CustomStringConvertible {
+    func toString() -> String {
+        "\(self)"
+    }
+}
