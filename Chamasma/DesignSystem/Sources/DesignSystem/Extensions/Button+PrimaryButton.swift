f2cef194238c5820fb5b4615d7b2b1f2268be8c3
Giga Khizanishvili <giga.khizanishvili@space.ge>
2023-06-02 16:31:11 +0400

    Update PrimaryButton

--- Chamasma/DesignSystem/Sources/DesignSystem/Extensions/Button+PrimaryButton.swift
+++ Chamasma/DesignSystem/Sources/DesignSystem/Extensions/Button+PrimaryButton.swift
@@ -1,25 +1,30 @@
 //
 //  Button+PrimaryModifier.swift
-//  
+//
 //
 //  Created by Giga Khizanishvili on 02.06.23.
 //
 
 import SwiftUI
 
+// TODO: Fix so that it is clickable on the whole width
 struct PrimaryButtonModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .foregroundColor(.blue)
-            .font(.title)
-            .padding()
-            .background(Color.yellow)
-            .cornerRadius(8)
+            .contentShape(Rectangle())
+            .font(.title3)
+            .foregroundColor(.white)
+            .frame(height: 50)
+            .frame(maxWidth: .infinity)
+            .background(DesignSystem.Color.hex4ED199())
+            .contentShape(Rectangle())
+            .frame(maxWidth: .infinity)
+            .cornerRadius(10)
     }
 }
 
-extension Button {
-    func makePrivateButton() -> some View {
+public extension Button {
+    func makePrimary() -> some View {
         modifier(PrimaryButtonModifier())
     }
 }
