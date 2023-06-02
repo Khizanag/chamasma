9e6eca1e2ae10dbf77c21abfe21bdb463ce44e38
Giga Khizanishvili <giga.khizanishvili@space.ge>
2023-06-02 12:09:57 +0400

    Create String.substring -> String extension

---
+++ Chamasma/Common/Sources/Common/Extensions/String.SubSequence+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//
+//
+//  Created by Giga Khizanishvili on 02.06.23.
+//
+
+public extension String.SubSequence {
+    func toString() -> String {
+        "\(self)"
+    }
+}
