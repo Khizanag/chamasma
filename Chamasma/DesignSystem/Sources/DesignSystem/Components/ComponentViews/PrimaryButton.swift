d3d2f7177a81daaad8a2e00cb41562d0d987fb87
Giga Khizanishvili <giga.khizanishvili@space.ge>
2023-06-28 03:14:34 +0400

    Create PrimaryButton

---
+++ Chamasma/DesignSystem/Sources/DesignSystem/Components/ComponentViews/PrimaryButton.swift
@@ -0,0 +1,52 @@
+//
+//  PrimaryButton.swift
+//
+//
+//  Created by Giga Khizanishvili on 02.06.23.
+//
+
+import SwiftUI
+
+public struct PrimaryButton: View {
+    // MARK: - Properties
+    let icon: Image?
+    let title: String
+    let action: () -> Void
+
+    // MARK: - Init
+    public init(icon: Image?, title: String, action: @escaping () -> Void) {
+        self.icon = icon
+        self.title = title
+        self.action = action
+    }
+
+    // MARK: - Body
+    public var body: some View {
+        Button(action: action) {
+            HStack(spacing: 10) {
+                Spacer()
+
+                if let icon {
+                    icon
+                }
+
+                Text(title)
+                    .contentShape(Rectangle())
+
+                Spacer()
+            }
+            .foregroundColor(.white)
+            .padding()
+            .contentShape(Rectangle())
+        }
+        .frame(maxWidth: .infinity)
+        .background(Color.blue)
+        .cornerRadius(10)
+        .contentShape(Rectangle())
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.clear)
+                .background(Color.clear)
+        )
+    }
+}
